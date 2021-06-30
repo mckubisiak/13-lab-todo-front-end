8214e4b576ac1d0da368c4fa368b8b89d232fec2
Kubi Kubisiak <mckubisiak@gmail.com>
2021-06-30 16:10:41 -0700

    create intial routes and corsponding pages with filler text

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import HomePage from "./HomePage";
+import SignupPage from "./SignupPage";
+import LoginPage from "./LoginPage";
+import TodoList from "./TodoList";
+
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <h2>Header</h2>
+          <Switch>
+            <Route 
+              path="/" 
+              exact
+              render={(routerProps) => <HomePage {...routerProps} />} 
+            />    
+            <Route 
+              path="/signup" 
+              exact
+              render={(routerProps) => <SignupPage {...routerProps} />} 
+            />    
+            <Route 
+              path="/login" 
+              exact
+              render={(routerProps) => <LoginPage {...routerProps} />} 
+            />          
+            <Route 
+              path="/todolist" 
+              exact
+              render={(routerProps) => <TodoList {...routerProps} />} 
+            />          
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
-export default App;
